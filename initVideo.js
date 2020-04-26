3fe33dcd93891d76cd0094948fb4f952a9bd4957
Nikolay Dozmorov <ndozmorov@gmail.com>
2020-04-26 17:55:02 +0700

    Add realization of brightness and contrast filters

--- initVideo.js
+++ initVideo.js
@@ -1,6 +1,6 @@
 function initVideo(video, url) {
     if (Hls.isSupported()) {
-        var hls = new Hls();
+        const hls = new Hls();
         hls.loadSource(url);
         hls.attachMedia(video);
         hls.on(Hls.Events.MANIFEST_PARSED, function () {
@@ -19,21 +19,65 @@ function initVideo(video, url) {
         if (!video.classList.contains("video--fullscreen")) {
             video.classList.add('video--fullscreen');
 
-            toggleControlsVisibility()
+            toggleControlsVisibility();
+
+            getContrastInput().value = 100;
+            getBrightnessInput().value = 100;
         }
-    })
+    });
+}
+
+
+function initControls() {
+    getBackButton().addEventListener('click', function() {
+        toggleControlsVisibility();
+
+        setCurrentVideoFilters(100, 100);
+        getCurrentVideo().classList.remove('video--fullscreen');
+    });
+
+    function qualityControlListener() {
+        const contrastInput = getContrastInput();
+        const brightnessInput = getBrightnessInput();
+
+        const contrast = contrastInput.value;
+        const brightness = brightnessInput.value;
+
+        setCurrentVideoFilters(contrast, brightness);
+    }
+
+    getContrastInput().addEventListener('input', qualityControlListener);
+    getBrightnessInput().addEventListener('input', qualityControlListener);
+}
+
+function getControls() {
+    return document.querySelector('.controls');
+}
+
+function getBackButton() {
+    return document.querySelector('.back-button')
+}
+
+function getContrastInput() {
+    return document.getElementById('contrast');
+}
+
+function getBrightnessInput() {
+    return document.getElementById('brightness');
+}
+
+function getCurrentVideo() {
+    return document.querySelector('.video.video--fullscreen');
+}
+
+function setCurrentVideoFilters(contrast, brightness) {
+    const currentVideo = getCurrentVideo();
+
+    if (currentVideo) {
+        currentVideo.style.filter = `contrast(${contrast}%) brightness(${brightness}%)`;
+    }
 }
 
 function toggleControlsVisibility() {
-    const controls = document.querySelector('.controls');
-    controls.classList.toggle('controls--visible');
-}
-
-function initControls() {
-    const controls = document.querySelector('.controls');
-
-    controls.querySelector('.back-button').addEventListener('click', function() {
-        toggleControlsVisibility();
-        document.querySelectorAll('.video').forEach(video => video.classList.remove('video--fullscreen'));
-    })
+    getControls().classList.toggle('controls--visible');
 }
